fb67cd7448c1771767474b5fcc0c1a7a9cc2ca88
shrutic640 <shruti.chatz@gmail.com>
2021-07-14 20:26:57 +1000

    Updated code

---
+++ Module1/Task1.1P.ino
@@ -0,0 +1,26 @@
+// C++ code
+//
+int sensorState = 0;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(12, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  // read the state of the sensor/digital input
+  sensorState = digitalRead(2);
+  // check if sensor pin is HIGH. if it is, set the
+  // LED on.
+  if (sensorState == HIGH) {
+    digitalWrite(12, HIGH);
+    Serial.println("Sensor activated!");
+  } else {
+    digitalWrite(12, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
---
+++ Module2/save-code.bat
@@ -0,0 +1,5 @@
+git remote add origin https://github.com/shrutic640/SIT315.git
+git add .
+git commit -am "Updated code"
+git push origin master
+PAUSE
